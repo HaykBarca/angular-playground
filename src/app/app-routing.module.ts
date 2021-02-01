cbb1cbd92edd0810777acca538e47257c88fbd20
Hayk Ghonakhchyan <h.ghonakhchyan@yahoo.com>
2021-02-01 13:25:38 +0400

    route preload strategies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadingStrategy, PreloadAllModules } from '@angular/router';
+import { QuicklinkModule, QuicklinkStrategy } from 'ngx-quicklink';
 
 const routes: Routes = [
   {
     path: 'playground',
     loadChildren: () => import('./playground/playground.module').then((m) => m.PlaygroundModule),
   },
-  { path: 'lazy-page', loadChildren: () => import('./lazy/lazy.module').then(m => m.LazyModule) },
+  { path: 'lazy-page', loadChildren: () => import('./lazy/lazy.module').then((m) => m.LazyModule) },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  imports: [
+    QuicklinkModule,
+    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy', preloadingStrategy: QuicklinkStrategy }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
