00491e544aea2b455634298f1f050e2052d09424
Hayk Ghonakhchyan <h.ghonakhchyan@yahoo.com>
2021-05-26 12:23:25 +0400

    ts

--- src/app/playground/typescript/typescript.component.ts
+++ src/app/playground/typescript/typescript.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-typescript',
   templateUrl: './typescript.component.html',
-  styleUrls: ['./typescript.component.scss']
+  styleUrls: ['./typescript.component.scss'],
 })
 export class TypescriptComponent implements OnInit {
+  private points: PointModel = {
+    x: 789,
+    y: 887,
+  };
 
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit(): void {
+    console.log(this.multiplePointsAndDivide(this.points.x, this.points.y, 3));
   }
 
+  private multiplePointsAndDivide(x: number, y: number, divider: 2 | 3): number {
+    return (x * y) / divider;
+  }
+
+}
+
+interface PointXModel {
+  x: number;
+}
+
+interface PointModel extends PointXModel {
+  y: number;
 }
